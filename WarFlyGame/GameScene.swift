5e9bbac35bbdfcdebd1fa0632f2b938ff04cb0a5
Rus <de.mr44@gmail.com>
2021-06-17 12:57:22 +0500

    Initial commit

---
+++ WarFlyGame/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  WarFlyGame
+//
+//  Created by ruslan on 16.06.2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ WarFlyGame/GameViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GameViewController.swift
+//  WarFlyGame
+//
+//  Created by ruslan on 16.06.2021.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size: self.view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
