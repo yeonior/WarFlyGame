9b69011d1aa7c7c16bd6c1522f3db8890a5c5c60
Rus <de.mr44@gmail.com>
2021-06-26 16:31:39 +0500

    Added enemy object

---
+++ WarFlyGame/Enemy.swift
@@ -0,0 +1,26 @@
+//
+//  Enemy.swift
+//  WarFlyGame
+//
+//  Created by ruslan on 26.06.2021.
+//
+
+import SpriteKit
+
+class Enemy: SKSpriteNode {
+    
+    static var textureAtlas: SKTextureAtlas?
+    
+    init() {
+        let texture = Enemy.textureAtlas?.textureNamed("airplane_4ver2_13")
+        super.init(texture: texture, color: .clear, size: CGSize(width: 221, height: 204))
+        self.xScale = 0.5
+        self.yScale = -0.5
+        self.zPosition = 20
+        self.name = "sprite"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }    
+}
