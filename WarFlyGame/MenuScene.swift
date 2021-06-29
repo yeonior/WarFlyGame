4a252d155cdb90c8654a1bd0da0ceeba8d758811
Rus <de.mr44@gmail.com>
2021-06-29 23:53:44 +0500

    Added menu scene

---
+++ WarFlyGame/MenuScene.swift
@@ -0,0 +1,33 @@
+//
+//  MenuScene.swift
+//  WarFlyGame
+//
+//  Created by ruslan on 29.06.2021.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1)
+        let texture = SKTexture(imageNamed: "play")
+        let button = SKSpriteNode(texture: texture)
+        button.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        button.name = "runButton"
+        self.addChild(button)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first?.location(in: self)
+        let node = self.atPoint(location!)
+        
+        if node.name == "runButton" {
+            let transition = SKTransition.crossFade(withDuration: 1)
+            let gameScene = GameScene(size: self.size)
+            gameScene.scaleMode = .aspectFill
+            self.scene?.view?.presentScene(gameScene, transition: transition)
+        }
+    }
+}
