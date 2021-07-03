1dcc5a2b1153c9471b032f7301862b5ab98bbc6e
Rus <de.mr44@gmail.com>
2021-07-03 19:05:50 +0500

    Upgraded menu interface

---
+++ WarFlyGame/Classes/ButtonNode.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonNode.swift
+//  WarFlyGame
+//
+//  Created by ruslan on 03.07.2021.
+//
+
+import SpriteKit
+
+class ButtonNode: SKSpriteNode {
+    
+    let label: SKLabelNode = {
+        
+        let l = SKLabelNode(text: "something")
+        l.fontColor = UIColor(red: 219 / 255, green: 226 / 255, blue: 215 / 255, alpha: 1.0)
+        l.fontName = "AmericanTypewriter-Bold"
+        l.fontSize = 30
+        l.horizontalAlignmentMode = .center
+        l.verticalAlignmentMode = .center
+        l.zPosition = 2
+        
+        return l
+    }()
+    
+    init(titled title: String, backgroundName: String) {
+        let texture = SKTexture(imageNamed: backgroundName)
+        super.init(texture: texture, color: .clear, size: texture.size())
+        label.text = title.uppercased()
+        addChild(label)        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
